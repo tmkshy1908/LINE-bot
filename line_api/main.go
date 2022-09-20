0aa912e614cf109884868cd588ae57bfc03a69ad
yamadatarou <taiko1okrock@gmail.com>
2022-09-20 21:03:44 +0900

    text if 追加

--- line_api/main.go
+++ line_api/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/line/line-bot-sdk-go/linebot"
 )
@@ -35,13 +36,30 @@ func lineHandler(w http.ResponseWriter, r *http.Request) {
 	for _, event := range events {
 		if event.Type == linebot.EventTypeMessage {
 			switch message := event.Message.(type) {
+
+			//テキストの場合
 			case *linebot.TextMessage:
 				replyMessage := message.Text
-				_, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do()
-				if err != nil {
-					log.Print(err)
+				if strings.Contains(replyMessage, "作成") {
+					replyMessage := "作成します"
+					bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do()
+				} else if strings.Contains(replyMessage, "更新") {
+					replyMessage := "更新します"
+					bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do()
+				} else if strings.Contains(replyMessage, "削除") {
+					replyMessage := "削除します"
+					bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do()
+				} else if strings.Contains(replyMessage, "取得") {
+					replyMessage := "取得します"
+					bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do()
+				} else {
+					_, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do()
+					if err != nil {
+						log.Print(err)
+					}
 				}
 
+			//スタンプの場合
 			case *linebot.StickerMessage:
 				replyMessage := "良いスタンプだねぇ"
 				_, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do()
@@ -49,6 +67,7 @@ func lineHandler(w http.ResponseWriter, r *http.Request) {
 					log.Print(err)
 				}
 
+			//画像の場合
 			case *linebot.ImageMessage:
 				replyMessage := "素敵な写真だなぁ"
 				_, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do()
