669f2ad9508e3a7f98ae987097b260dae6f5564f
tmkshy1908 <112471018+tmkshy1908@users.noreply.github.com>
2022-09-27 19:52:33 +0900

    * データベス、Create Delete 追加
    ##タイトル
    データベースの追加、Create Deleteの追加
    
    ##目的
    データベスを扱うための下処理
    データベースにデータを挿入する処理のテスト
    データベースのデータを削除する処理のテスト
    
    
    ##達成できる内容
    ・特定のテキストを受け取った場合にデータを挿入、削除させる

--- line_api/main.go
+++ line_api/main.go
@@ -1,12 +1,32 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
+
+	_ "github.com/lib/pq"
 
 	"github.com/line/line-bot-sdk-go/linebot"
 )
+
+type Schedule struct {
+	Id       int
+	Day      int
+	Contents string
+}
+
+var Db *sql.DB
+
+func init() {
+	var err error
+	Db, err = sql.Open("postgres", "usr= dbname= password=")
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
 	http.HandleFunc("/calback", lineHandler)
@@ -35,13 +55,36 @@ func lineHandler(w http.ResponseWriter, r *http.Request) {
 	for _, event := range events {
 		if event.Type == linebot.EventTypeMessage {
 			switch message := event.Message.(type) {
+
+			//テキストの場合
 			case *linebot.TextMessage:
 				replyMessage := message.Text
-				_, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do()
-				if err != nil {
-					log.Print(err)
+				if strings.Contains(replyMessage, "作成") {
+					replyMessage := "作成します"
+					bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do()
+					schedule := Schedule{Day: 20220101, Contents: "テスト"}
+					schedule.Create()
+					replyMessage = "作成しました"
+					bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do()
+				} else if strings.Contains(replyMessage, "更新") {
+					replyMessage := "更新します"
+					bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do()
+				} else if strings.Contains(replyMessage, "削除") {
+					replyMessage := "削除します"
+					bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do()
+					schedule := Schedule{Id: 1}
+					schedule.Delete()
+				} else if strings.Contains(replyMessage, "取得") {
+					replyMessage := "取得します"
+					bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do()
+				} else {
+					_, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do()
+					if err != nil {
+						log.Print(err)
+					}
 				}
 
+			//スタンプの場合
 			case *linebot.StickerMessage:
 				replyMessage := "良いスタンプだねぇ"
 				_, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do()
@@ -49,6 +92,7 @@ func lineHandler(w http.ResponseWriter, r *http.Request) {
 					log.Print(err)
 				}
 
+			//画像の場合
 			case *linebot.ImageMessage:
 				replyMessage := "素敵な写真だなぁ"
 				_, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do()
@@ -58,4 +102,14 @@ func lineHandler(w http.ResponseWriter, r *http.Request) {
 			}
 		}
 	}
+}
+
+func (schedule *Schedule) Create() (err error) {
+	err = Db.QueryRow("insert into schedule (day, contents) values ($1, $2) returning id", schedule.Day, schedule.Contents).Scan(&schedule.Id)
+	return
+}
+
+func (schedule *Schedule) Delete() (err error) {
+	_, err = Db.Exec("delete from schedule where id = $1", schedule.Id)
+	return
 }
