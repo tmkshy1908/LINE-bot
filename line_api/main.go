34b229e3012128815ec3181fed48c29e546adad6
tmkshy1908 <112471018+tmkshy1908@users.noreply.github.com>
2022-10-18 09:03:00 +0900

    create修正 (#9)

--- line_api/main.go
+++ line_api/main.go
@@ -23,9 +23,9 @@ var Db *sql.DB
 
 func init() {
 	var err error
-	Db, err = sql.Open("postgres", "usr= dbname= password=")
+	Db, err = sql.Open("postgres", "user= dbname= password= sslmode=disable")
 	if err != nil {
-		panic(err)
+		fmt.Println("Db.Openエラー:", err)
 	}
 }
 
@@ -41,7 +41,7 @@ func lineHandler(w http.ResponseWriter, r *http.Request) {
 		"アクセストークン",
 	)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println("linebot.Newエラー:", err)
 	}
 
 	events, err := bot.ParseRequest(r)
@@ -63,7 +63,7 @@ func lineHandler(w http.ResponseWriter, r *http.Request) {
 				if strings.Contains(replyMessage, "作成") {
 					replyMessage := "作成します"
 					bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do()
-					schedule := Schedule{Day: 20220101, Contents: "テスト"}
+					schedule := Schedule{Id: 1, Day: 20220101, Contents: "テスト"}
 					schedule.Create()
 					replyMessage = "作成しました"
 					bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do()
@@ -71,7 +71,7 @@ func lineHandler(w http.ResponseWriter, r *http.Request) {
 				} else if strings.Contains(replyMessage, "更新") {
 					replyMessage := "更新します"
 					bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do()
-					schedule := Schedule{Day: 20220101, Contents: "アップデートしました"}
+					schedule := Schedule{Id: 1, Day: 20220101, Contents: "アップデートしました"}
 					schedule.Update()
 					replyMessage = "更新しました"
 					bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do()
@@ -116,22 +116,34 @@ func lineHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (schedule *Schedule) Create() (err error) {
-	err = Db.QueryRow("insert into schedule (day, contents) values ($1, $2) returning id", schedule.Day, schedule.Contents).Scan(&schedule.Id)
+	_, err = Db.Exec("insert into schedule (id, day, contents) values($1,$2,$3)", schedule.Id, schedule.Day, schedule.Contents)
+	if err != nil {
+		fmt.Println("Create Execエラー:", err)
+	}
 	return
 }
 
 func (schedule *Schedule) Update() (err error) {
 	_, err = Db.Exec("update schedule set day = $2, contents = $3 where id = $1", schedule.Id, schedule.Day, schedule.Contents)
+	if err != nil {
+		fmt.Println(err)
+	}
 	return
 }
 
 func (schedule *Schedule) Delete() (err error) {
 	_, err = Db.Exec("delete from schedule where id = $1", schedule.Id)
+	if err != nil {
+		fmt.Println(err)
+	}
 	return
 }
 
 func GetSchedule(id int) (schedule Schedule, err error) {
 	schedule = Schedule{}
 	err = Db.QueryRow("select id, day, contents from schedule where id = &1", id).Scan(&schedule.Id, &schedule.Day, &schedule.Contents)
+	if err != nil {
+		fmt.Println(err)
+	}
 	return
 }
